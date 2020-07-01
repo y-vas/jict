4b1decf3743815070f62c57fa540197f8b37adc6
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2020-07-01 16:45:35 +0200

    debug

--- jict/implementation.py
+++ jict/implementation.py
@@ -238,8 +238,8 @@ class jict( defaultdict ):
             valid = ['smh://','sql://']
 
             for x in valid:
-                print(len(name) , len(x) , name[len(x):] , x)
-                if len(name) >= len(x) and name[len(x):] == x:
+                print(len(name) , len(x) , name[:len(x)] , x)
+                if len(name) >= len(x) and name[:len(x)] == x:
                     if name[:-len(x)] == 'sql://':
                         self.sql_store(name[:len(x)])
                         print(name[:len(x)])
