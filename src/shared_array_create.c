7043b18ee39bf38f16d0d1b98513a28cd9edcb71
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2020-07-01 23:35:07 +0200

    debug

--- src/shared_array_create.c
+++ src/shared_array_create.c
@@ -32,9 +32,10 @@ static PyObject *do_create(const char *name, int ndims, npy_intp *dims ) {
 
 	/* Calculate the memory size of the array */
 	size = dtype->elsize;
-	for (i = 0; i < ndims; i++)
+	for (i = 0; i < ndims; i++){
 		printf("%s", dims[i] ); // %s is format specifier
 		size *= dims[i];
+	}
 
 	/* Calculate the size of the mmap'd area */
 	map_size = size + sizeof (*meta);
