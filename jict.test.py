7d914385879b4a210f40bc56880784fe91eab90e
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2020-07-09 17:30:56 +0200

    debug

--- jict.test.py
+++ jict.test.py
@@ -7,3 +7,7 @@ jct['memoryfield'] = ['hi']
 jct2 = jict('shm://mymemory')
 print( jct2['memoryfield'] )
 # output : hi
+
+
+# python3 setup.py sdist bdist_wheel
+# twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
