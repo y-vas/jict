887745aad2ad8b1175666a996986abe2a710e595
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2020-07-10 09:44:43 +0200

    2.2.2

--- jict/__init__.py
+++ jict/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """`jict` provides dictionaries with multiple levels of nested-ness."""
 
-__version__ = '2.2.1'
+__version__ = '2.2.2'
 from .implementation import jict, sqlconnect, evaluate
 from collections import deque
 
