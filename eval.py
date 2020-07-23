fc05f642a0ed52cb26024334188bb7e0f5d7e004
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2020-07-23 12:35:45 +0200

    debug

--- eval.py
+++ eval.py
@@ -1,15 +1,10 @@
-from eval.base import eval
-
+from eval import eval_base
 from jict import jict
 
-jct = jict({"1":{"name":"bob","age":"20","work":"Assistant"}})
-jct2 = jict({"2":{"name":"James","age":"36","work":"Dev"}})
-
-jct['1'].replace({"name":"bob","age":"25","work":"Dev"})
-jct2['2'].replace({"name": "James", "age": "40", "work": "Assistant"})
-
-print(jct)
-print(jct2)
+# jct = jict({"1":{"name":"bob","age":"20","work":"Assistant"}})
+# jct2 = jict({"2":{"name":"James","age":"36","work":"Dev"}})
+# jct.replace({"name":"bob","age":"25","work":"Dev"})
+# jct2.replace({"name": "James", "age": "40", "work": "Assistant"})
 
 jct = jict()
-# base( jct )
+eval_base( jct )
--- eval/base.py
+++ eval/base.py
@@ -1,21 +1,24 @@
 
-def eval(jct):
+def eval_base(jct):
     jct['base'] = 1
-
     if jct['base'] != 1:
         raise Exception(' base is not == 1 :', jct['base'])
 
     jct['base2']['sub'] = 1
-
     if jct['base2']['sub'] != 1:
         raise Exception(' base2.sub is not == 1 :', jct['base2']['sub'])
 
     jct.init('base', 5 )
-
     if jct['base'] != 1:
         raise Exception(' init not set properly :', jct['base'])
 
     jct.init('base3', 5 )
-
     if jct['base3'] != 5:
         raise Exception(' init not set properly :', jct['base3'])
+
+
+    # test iad
+    # default dict 
+
+
+    print( jct )
