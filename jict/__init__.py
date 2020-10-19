a05d1c28ed3ab3fcf7d1d24d2342d282f342a595
Vasyl Yovdiy <yovdiyvasyl@gmail.com>
2020-10-19 10:23:40 +0200

    debug

--- jict/__init__.py
+++ jict/__init__.py
@@ -2,7 +2,7 @@
 
 """`jict` provides dictionaries with multiple levels of nested-ness."""
 
-__version__ = '2.7'
+__version__ = '2.8'
 from .jict import jict
 from .helpers import evaluate , sqlconnect
 from collections import deque
